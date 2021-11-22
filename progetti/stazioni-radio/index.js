d617cf77afa50f750eeb3d6d20ac6b0147ef3bf8
Nicolò Carpignoli <nicolocarpignoli@gmail.com>
2021-11-22 09:03:54 +0100

    Update index.js

--- progetti/stazioni-radio/index.js
+++ progetti/stazioni-radio/index.js
@@ -6,6 +6,8 @@ const MIN_DISTANCE = 50;
 const MAX_NUMBER_MARKERS = 10;   // not limiting, for now
 const TIME_TO_UPDATE = 30;    // in seconds
 
+var closestEntity = null;
+
 // Calculate distance between two positions (with latitude and longitude fields)
 function computeDistance(position1, position2) {
     var dlongitude = THREE.Math.degToRad(parseFloat(position1.longitude) - parseFloat(position2.longitude));
@@ -317,6 +319,7 @@ window.addEventListener('load', () => {
         camera.setAttribute('gps-camera', 'simulateLatitude', latitude);
         camera.setAttribute('gps-camera', 'simulateLongitude', longitude);
         window.currentPosition = { latitude, longitude };
+        camera.update();
         const places = findClosestPlaces(window.currentPosition, window.places, MAX_NUMBER_MARKERS);
         renderPlaces(places);
         setClosestPlaceOnFooter();
