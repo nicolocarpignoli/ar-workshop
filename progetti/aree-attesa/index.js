4bb7ee0f051bb9f6d5f90ff90ccbec073c62a490
Nicolò Carpignoli <nicolocarpignoli@gmail.com>
2021-11-22 09:03:21 +0100

    Update index.js

--- progetti/aree-attesa/index.js
+++ progetti/aree-attesa/index.js
@@ -318,6 +318,7 @@ window.addEventListener('load', () => {
         camera.setAttribute('gps-camera', 'simulateLatitude', latitude);
         camera.setAttribute('gps-camera', 'simulateLongitude', longitude);
         window.currentPosition = { latitude, longitude };
+        camera.update();
         const places = findClosestPlaces(window.currentPosition, window.places, MAX_NUMBER_MARKERS);
         renderPlaces(places);
         setClosestPlaceOnFooter();
